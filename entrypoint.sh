be7a8cf3d5b910882515a36c11fa62d759419a8e
ryan-dia <cjfdnjs158@naver.com>
2025-07-18 10:50:41 +0900

    entrypoint 변경

--- entrypoint.sh
+++ entrypoint.sh
@@ -10,5 +10,5 @@ chown -R ${APP_UID}:${APP_GID} /maildir
 # (3) app 유저에게 읽기/쓰기/실행 권한 부여
 chmod -R u+rwx /maildir
 
-# (4) 실제 자바 애플리케이션 실행
-exec java -jar /app/app.jar --spring.profiles.active=docker
+# (3) app 유저로 애플리케이션 실행
+exec su-exec ${APP_USER} java -jar /app/app.jar --spring.profiles.active=docker
